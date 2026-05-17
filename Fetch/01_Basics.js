e6a72744fb321146b531a7833707a05afdddc5f7
unknown <devanshudeshmukh04@gmail.com>
2026-05-17 14:06:03 +0530

    13-commit

---
+++ Fetch/01_Basics.js
@@ -0,0 +1,2 @@
+//  fetch function is used as the global method starts the process of fetching a resourse from the network returing a promise which is fullfillerd 
+// once the response is available 
